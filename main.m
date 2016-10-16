43da048eb07babb6d77a1aa09f53de2edbf880f3
LillyTraudl <ltmeg@hotmail.de>
2016-10-16 12:48:18 +0200

    documentation
    - I used his assignment.tex -> just delete, what you do not want to have
    in the documentation at the end
    - figures are in the figures folder -> just to have less files in the
    main folder
    - I've put my answer always directly below the task item

--- main.m
+++ main.m
@@ -12,17 +12,30 @@ clc
 data = dlmread('perceptrondataUseful.csv',','); 
 
 % plot the input vectors in R^2
-figure()
+figure(1)
 scatter(data(:,1),data(:,2),20,data(:,3),'filled')
 xlabel('x')
 ylabel('y')
 title('input vectors')
+% save the figure
+set(1,'Units','Inches');
+pos = get(1,'Position');
+set(1,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(1,'figures/inputVectors','-dpdf','-r0')
+
 
 % do feature transformation and plot again
 transformed_data = [data(:,1:2).^2 data(:,3)];
-figure()
+figure(2)
 scatter(transformed_data(:,1),transformed_data(:,2),20,transformed_data(:,3),'filled')
 xlabel('x')
 ylabel('y')
 title('transformed input vectors')
+% save the figure
+set(2,'Units','Inches');
+pos = get(2,'Position');
+set(2,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(2,'figures/transformedInputVectors','-dpdf','-r0')
+
+
 %% Task 1.1.2
