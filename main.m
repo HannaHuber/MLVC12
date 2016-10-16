f315e8f2bf542602dedb211f36a9530b87b50712
LillyTraudl <ltmeg@hotmail.de>
2016-10-16 11:49:23 +0200

    1.1.1 plot input and transformed input
    Plotted the data and the transformed data with scatter
    at the moment figures are not saved and no documentation is written

--- main.m
+++ main.m
@@ -4,9 +4,25 @@
 % Hanna Huber
 
 clear all
+close all
 clc
 
 %% Task 1.1.
+% read in data -> new .csv file written by .py-file
 data = dlmread('perceptrondataUseful.csv',','); 
 
+% plot the input vectors in R^2
+figure()
+scatter(data(:,1),data(:,2),20,data(:,3),'filled')
+xlabel('x')
+ylabel('y')
+title('input vectors')
+
+% do feature transformation and plot again
+transformed_data = [data(:,1:2).^2 data(:,3)];
+figure()
+scatter(transformed_data(:,1),transformed_data(:,2),20,transformed_data(:,3),'filled')
+xlabel('x')
+ylabel('y')
+title('transformed input vectors')
 %% Task 1.1.2
