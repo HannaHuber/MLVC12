2695d645dd75d6054d1512869cfd74d452e170f6
HannaHuber <hanna_huber@hotmail.com>
2016-12-27 22:13:51 +0100

    create dim x numSamples training data matrix

--- Ass2/src/generateTrainingData.m
+++ Ass2/src/generateTrainingData.m
@@ -1,24 +1,21 @@
-function data = generateTrainingData(N, xRange, yRange, linear)
+function [X, t] = generateTrainingData(N, xRange, yRange, linear)
     
     % generate N random 2D samples 
-    X = rand(N, 2);
+    X = rand(2, N);
     
     % map from (0,1) to specified range
-    X(:,1) = repmat(xRange(1), N, 1) + X(:,1)*(xRange(2) - xRange(1));
-    X(:,2) = repmat(yRange(1), N, 1) + X(:,2)*(yRange(2) - yRange(1));
+    X(1, :) = repmat(xRange(1), 1, N) + X(1, :)*(xRange(2) - xRange(1));
+    X(2, :) = repmat(yRange(1), 1, N) + X(2, :)*(yRange(2) - yRange(1));
 
     % create target labels for linearly separable data
     if linear
-        labels = int16(sum(X,2) > mean(xRange) + mean(yRange));
-        labels(labels<1) = -1;
+        t = double(sum(X) > mean(xRange) + mean(yRange))';
+        t(t<1) = -1;
         
     % create target labels for general data
     else
         % TODO
         
     end
-    
-    % assign labels to data points
-    data = [ X labels];
-    
+       
 end
